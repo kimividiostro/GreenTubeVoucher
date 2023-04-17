0ab3340c0cb316229324578bec562897609b50ec
Milan <milance@pop-os.localdomain>
2023-04-17 15:37:41 +0200

    Added missing fields to Location model

--- src/app/Components/Voucher/Models/Location.ts
+++ src/app/Components/Voucher/Models/Location.ts
@@ -1,7 +1,12 @@
 export interface Location {
+    id: string,
     name: string,
+    lat: string,
+    long: string,
     address: string,
     city: string,
     postcode: string,
+    telephone: string,
+    link: string,
     hours: string
 }
